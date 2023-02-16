055eda70badf0cdafd9a28683350b982862561c5
Yonas Alemayehu <yonialemye@gmail.com>
2023-02-17 00:31:56 +0300

    animating theme change

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:afric_shop_app/app/core/constants/app_sizes.dart';
 import 'package:afric_shop_app/app/features/onboard/screens/welcome_screen.dart';
-import 'package:afric_shop_app/app/themes/app_themes.dart';
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'app/themes/theme_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final themeRepo = await ThemeRepository.instance;
+  runApp(
+    MainApp(initTheme: themeRepo.initial),
+  );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required ThemeData initTheme}) : _initTheme = initTheme;
+
+  final ThemeData _initTheme;
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: AppSizes.designSize,
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: const WelcomeScreen(),
-      builder: (_, home) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Afric Shop',
-          theme: AppThemes.lightMode(),
-          darkTheme: AppThemes.darkMode(),
-          home: home,
+    return ThemeProvider(
+      initTheme: _initTheme,
+      builder: (_, theme) {
+        return ScreenUtilInit(
+          designSize: AppSizes.designSize,
+          minTextAdapt: true,
+          splitScreenMode: true,
+          child: const WelcomeScreen(),
+          builder: (_, home) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Afric Shop',
+              theme: theme,
+              home: home,
+            );
+          },
         );
       },
     );
